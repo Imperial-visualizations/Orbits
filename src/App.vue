eaf6bc3f5617f7258807f4c2d4172a358e4fc287
Robert Jones <Puredbest3@gmail.com>
2020-08-09 12:23:20 +0200

    Actual Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <iv-title-bar />
+    <iv-visualisation>
+      <template #hotspots>
+        <iv-pane position="left" format="full"/>
+        <iv-pane position="right" format="overlay">
+            
+        </iv-pane>
+        <!-- <iv-toggle-hotspot id="iv-toggle-hotspot-bottomleft" position="bottomleft" title="BOTTOM LEFT"></iv-toggle-hotspot> -->
+        <!-- <iv-toggle-hotspot id="iv-toggle-hotspot-topleft" position="topleft" title="TOP LEFT"></iv-toggle-hotspot> -->
+
+        <iv-toggle-hotspot id="iv-fixed-hotspot-bottom" position="bottom" title="Effective Potential">
+            <!-- <EffPot ref="effPlot" /> -->
+        </iv-toggle-hotspot>
+        <!-- <iv-fixed-hotspot id="iv-fixed-hotspot-left" position="left" title="LEFT"></iv-fixed-hotspot> -->
+        <!-- <iv-fixed-hotspot id="iv-fixed-hotspot-right" position="right" title="RIGHT"></iv-fixed-hotspot> -->
+        
+      </template>
+
+      <template #main-stage>
+        <Orbits @energies="updatePlot"  @onresize="updateSize" />
+        <!-- <EffPot ref="effPlot" /> -->
+      </template>
+
+      <iv-guidance-button guidanceIdentifier="A1"/>
+    </iv-visualisation>
+    <iv-guidance-system :guidance_branches_input="guidance_branch_list" />
+  </div>
+</template>
+
+<script>
+import Orbits from './components/Orbits.vue'; 
+import EffPot from './components/EffectivePotential.vue';
+//<iv-pane position="left" />
+//
+//
+export default {
+  name:"App",
+  components:{
+    Orbits,
+    EffPot
+  },
+  data(){
+    return{
+      guidance_branch_list:[
+        {title:"Branch 1",
+          branch_data:[
+              {text:"A text 1", highlightDiv:"iv-fixed-hotspot-topright" ,identifier: "A1", positionModal:[4,8,2,5]},
+              {text:"A text 2", highlightDiv:"iv-toggle-hotspot-bottomright" ,identifier: "A2", positionModal:[4,8,7,9]},
+              {text:"A text 3", highlightDiv:"iv-toggle-hotspot-bottom" ,identifier: "A3", positionModal:[4,8,5,7]}
+          ]
+        },
+        {title:"Branch 2",
+          branch_data:[
+              {text:"B text 1", highlightDiv:"iv-toggle-hotspot-bottomright" ,identifier: "B1", positionModal:[4,8,7,9]},
+              {text:"B text 2", highlightDiv:"iv-toggle-hotspot-bottomright" ,identifier: "B2", positionModal:[4,8,7,9]},
+              {text:"B text 3", highlightDiv:"iv-toggle-hotspot-bottomright" ,identifier: "B3", positionModal:[4,8,7,9]}
+          ]
+        },
+        {title:"Branch 3",
+          branch_data:[
+              {text:"C text 1", highlightDiv:"iv-toggle-hotspot-bottom" ,identifier: "C1", positionModal:[4,8,3,5]},
+              {text:"C text 2", highlightDiv:"iv-toggle-hotspot-bottom" ,identifier: "C2", positionModal:[4,8,3,5]},
+              {text:"C text 3", highlightDiv:"iv-toggle-hotspot-bottom" ,identifier: "C3", positionModal:[4,8,3,5]}
+          ]
+        },
+      ]
+    }
+  },
+  methods:{
+    updatePlot(energies){
+      console.log('updatePlotParent');
+      this.$refs.effPlot.updatePlot(energies);     
+    },
+    updateSize(){
+      console.log('resize');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+ 
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import ImpVis from '@impvis/components';
+import '@impvis/components/dist/impvis-components.css';
+
+Vue.use(ImpVis);
+Vue.config.productionTip = false;
+
+//export const bus = new Vue();//bus for title bar to talk to guidance
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
